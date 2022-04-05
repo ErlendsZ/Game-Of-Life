082b401326e0b91d9795cebbfe4e2b3245fc336b
Erlends Zagorskis <erlends.zagorskis>
2022-04-05 18:08:43 +0300

    Added random pollution of an array. UAdded userser selectable dimensions.

--- GameOfLife/Program.cs
+++ GameOfLife/Program.cs
@@ -2,31 +2,27 @@
 {
     static public void Main(string[] args)
     {
-        Game GameIterator = new Game();
-        GameIterator.CurrentField();
+        GameFieldSize gameFieldSize = new GameFieldSize();
+        int lenght = gameFieldSize.SelectLenght();
+        int width = gameFieldSize.SelectWidth();
 
-        PrintFieldIteration();
+        RandomCellGenerator randomStart = new RandomCellGenerator();
+        int [,] createdArr = randomStart.RandCells(new int[width, lenght]);
+
+        Console.WriteLine();
+
+        GameIterator gameIterator = new GameIterator();
+        gameIterator.PrintArrayIteration(createdArr);
+
     }
 
-    
-
-    static void PrintFieldIteration()
-    {
-        //Console.WriteLine("sds");
-    }
 }
 
-public class Game
-{   
+public class GameIterator { 
     //Iterating through field each second
-    public void CurrentField()
+    public void PrintArrayIteration(int[,] GameField)
     {
-        Console.WriteLine("Enter Lenght");
-        int lenght = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Enter Width");
-        int width = Convert.ToInt32(Console.ReadLine());
-        int[,] GameField = new int[width, lenght];
-
+      
         for (int i = 0; i < GameField.GetLength(0); i++)
         {
             for (int j = 0; j < GameField.GetLength(1); j++)
@@ -46,9 +42,53 @@ class Rules
    //Any live cell with more than three live neighbours dies, as if by overpopulation.
    //Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
 }
-class Field
+
+public class RandomCellGenerator
 {
-    //Field size 2D array
+    // Generates randomly which cells are alive which not
+    public int[,] RandCells(int[,] GameField)
+    {
+        Random rand = new Random();
+        
+
+        for (int i = 0; i < GameField.GetLength(0); i++)
+        {
+           
+            for (int j = 0; j < GameField.GetLength(1); j++)
+            {
+                int number = rand.Next(0, 2); //writes random number between 0-1
+                GameField[i, j] = number;
+                Console.Write(GameField[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+        int [,] pollutedArray = GameField;
+
+        return pollutedArray;
+    }
+
+}
+
+
+
+class GameFieldSize
+{
+    //Field size set up  2D array
+    public int SelectLenght()
+    {
+        Console.WriteLine("Enter Lenght");
+        int lenght = Convert.ToInt32(Console.ReadLine());
+        return lenght;
+    }
+
+    public int SelectWidth()
+    {
+        Console.WriteLine("Enter Lenght");
+        int lenght = Convert.ToInt32(Console.ReadLine());
+        return lenght;
+
+    }
+
 }
 
 class Cell
