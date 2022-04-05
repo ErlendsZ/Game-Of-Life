4a41ed7a708be1c19dc1dc74178164996189aae6
Erlends Zagorskis <erlends.zagorskis>
2022-04-05 12:27:35 +0300

    Ideas for game of life

---
+++ GameOfLife/Program.cs
@@ -0,0 +1,32 @@
+﻿public class Program
+{
+    static public void Main(string[] args)
+    {
+        SetFieldSize();
+        PrintFieldIteration();
+    }
+
+    static void SetFieldSize()
+    {
+        Console.WriteLine("Enter Lenght");
+        int lenght = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter Width");
+        int width = Convert.ToInt32(Console.ReadLine());
+        int[,] GameField = new int[width, lenght];
+
+        for (int i = 0; i < GameField.GetLength(0); i++)
+        {
+            for (int j = 0; j < GameField.GetLength(1); j++)
+            {
+                Console.Write(GameField[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static void PrintFieldIteration()
+    {
+        //Console.WriteLine("sds");
+    }
+}
+
