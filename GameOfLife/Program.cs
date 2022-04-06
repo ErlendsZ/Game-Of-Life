91b7e4cca613f333dffaab6ba96bf181b2ca9938
Erlends Zagorskis <erlends.zagorskis>
2022-04-06 10:51:04 +0300

    Added Iteration Displaying and CellCalculation template

--- GameOfLife/Program.cs
+++ GameOfLife/Program.cs
@@ -2,24 +2,41 @@
 {
     static public void Main(string[] args)
     {
-        GameFieldSize gameFieldSize = new GameFieldSize();
+        GameFieldSize gameFieldSize = new GameFieldSize();  // Set field size
         int lenght = gameFieldSize.SelectLenght();
         int width = gameFieldSize.SelectWidth();
 
-        RandomCellGenerator randomStart = new RandomCellGenerator();
-        int [,] createdArr = randomStart.RandCells(new int[width, lenght]);
-
+        RandomCellGenerator randomCellGenerator = new RandomCellGenerator();        // Generates Cells in field
+        int [,] gameArr = randomCellGenerator.RandCells(new int[width, lenght]);
         Console.WriteLine();
 
-        GameIterator gameIterator = new GameIterator();
-        gameIterator.PrintArrayIteration(createdArr);
+        Console.WriteLine("Starting field");
+        Console.WriteLine();
+        GameIterator gameIterator = new GameIterator();         // Prints Generated field 
+        gameIterator.PrintArrayIteration(gameArr);
+        Console.WriteLine();
+
+
+        for (int i = 0; i < 3; i++)
+        {
+            Console.WriteLine("Iteration " + (i+1));
+            Console.WriteLine();
+            Rules rules = new Rules();      // Change contents of field according to rules (1 itteration)
+            rules.CellCalculation(gameArr);
+          
+
+            gameIterator.PrintArrayIteration(gameArr);  // Prints changed field
+            Console.WriteLine();
+        }
+     
 
     }
 
 }
 
 public class GameIterator { 
-    //Iterating through field each second
+
+    //Iterating through field and displaying results
     public void PrintArrayIteration(int[,] GameField)
     {
       
@@ -41,6 +58,26 @@ class Rules
    //Any live cell with two or three live neighbours lives on to the next generation.
    //Any live cell with more than three live neighbours dies, as if by overpopulation.
    //Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
+   public int[,] CellCalculation(int [,] GameField) 
+    {
+        // NOT IMPLEMENTED, CALCULATION CODE GOES HERE
+        int[,] tempArr = GameField;
+
+        for (int i = 0; i < GameField.GetLength(0); i++)
+        {
+            for (int j = 0; j < GameField.GetLength(1); j++)
+            {
+                GameField[i, j] = 2;
+            }
+        }
+
+        return GameField;
+
+    }
+
+
+
 }
 
 public class RandomCellGenerator
@@ -56,15 +93,13 @@ public class RandomCellGenerator
            
             for (int j = 0; j < GameField.GetLength(1); j++)
             {
-                int number = rand.Next(0, 2); //writes random number between 0-1
+                int number = rand.Next(0, 2); // Writes random number between 0-1
                 GameField[i, j] = number;
-                Console.Write(GameField[i, j] + " ");
             }
-            Console.WriteLine();
-        }
-        int [,] pollutedArray = GameField;
 
-        return pollutedArray;
+        }
+
+        return GameField;
     }
      
 }
@@ -73,7 +108,7 @@ public class RandomCellGenerator
 
 class GameFieldSize
 {
-    //Field size set up  2D array
+    // Field size set up  2D array
     public int SelectLenght()
     {
         Console.WriteLine("Enter Lenght");
@@ -89,10 +124,4 @@ class GameFieldSize
 
     }
 
-}
-
-class Cell
-{
-    // isAlive or isNotAlive (1/0)
-    
 }
