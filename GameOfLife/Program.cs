67ab69476f6a60ebdf0c20e93dcbe8f23b2d2460
Erlends Zagorskis <erlends.zagorskis>
2022-04-06 17:30:05 +0300

    Updated CellCalculation method

--- GameOfLife/Program.cs
+++ GameOfLife/Program.cs
@@ -17,7 +17,7 @@
         Console.WriteLine();
 
 
-        for (int i = 0; i < 3; i++)
+        for (int i = 0; i < 9; i++)
         {
             Console.WriteLine("Iteration " + (i+1));
             Console.WriteLine();
@@ -62,18 +62,80 @@ class Rules
    public int[,] CellCalculation(int [,] GameField) 
     {
         // NOT IMPLEMENTED, CALCULATION CODE GOES HERE
-        int[,] tempArr = GameField;
 
-        for (int i = 0; i < GameField.GetLength(0); i++)
+        int[,] tempArr = (int[,])GameField.Clone(); 
+
+        int lenght = GameField.GetLength(0);
+        int width = GameField.GetLength(1);
+        //int [,] cordinatesArr = new int[,] { { -1, 0 }, { -1, 1 }, { 0, 1 }, { 1, 1 }, // One Cell can have a maximum 8 neighbours cell cordinates {0.0}
+        //    {1, 0 }, { 1, -1}, {0, -1 }, { -1, -1} };
+
+        int[] cordinatesArr = new int[] {  -1, 0 ,  -1, 1 , 0, 1 ,  1, 1 , // One Cell can have a maximum 8 neighbours cell cordinates {0.0}
+            1, 0, 1, -1, 0, -1 ,  -1, -1 };
+
+
+
+        for (int i = 0; i < lenght; i++)
         {
-            for (int j = 0; j < GameField.GetLength(1); j++)
+
+            for (int j = 0; j < width; j++)     // set copied same size array elements to 0
             {
-                GameField[i, j] = 2;
-            } 
+                tempArr[i, j] = 0;
+               
+            }
+
         }
 
-        return GameField;
 
+        for (int i = 0; i < lenght; i++)
+        {
+
+            for (int j = 0; j < width; j++)
+            {
+                int liveCells = 0;
+
+
+
+                for (int k = 0; k < 16; k = k + 2)
+                {
+                    int x = cordinatesArr[k];
+                    int y = cordinatesArr[k+1];
+  
+
+                    if (x >= 0 && x < lenght && y >= 0 && y < width && GameField[x, y] == 1)
+                    {
+                        liveCells++;
+
+                    }
+                }
+                
+    
+                if (GameField[i,j] == 0 && liveCells == 3)
+                {
+                    tempArr[i,j] = 1; 
+
+                } else if (GameField[i,j] == 1)
+                {
+                    if (liveCells == 2 || liveCells == 3)
+                    {
+                        tempArr[i, j] = 1;
+                    }
+                }
+          
+
+            } 
+        }
+        for (int i = 0; i < lenght; i++)
+        {
+
+            for (int j = 0; j < width; j++)
+            {
+
+            GameField[i,j] = tempArr[i,j];
+            }
+
+        }
+        return GameField;
     }
 
 
