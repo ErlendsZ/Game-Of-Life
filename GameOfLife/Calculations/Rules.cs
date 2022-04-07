e1d80a88c6c43437c1c2fea0b343312ac720cd62
Erlends Zagorskis <erlends.zagorskis>
2022-04-07 17:05:17 +0300

    Updated code formating

--- GameOfLife/Calculations/Rules.cs
+++ GameOfLife/Calculations/Rules.cs
@@ -8,24 +8,18 @@ namespace GameOfLife
 {
     class Rules
     {
-
         public int[,] CellCalculation(int[,] GameField)
         {
-
             int[,] tempArr = (int[,])GameField.Clone();
             int lenght = GameField.GetLength(0);
             int width = GameField.GetLength(1);
-            
 
             for (int i = 0; i < lenght; i++)
             {
-
                 for (int j = 0; j < width; j++)     // set copied same size array elements to 0
                 {
                     tempArr[i, j] = 0;
-
                 }
-
             }
 
 
@@ -33,55 +27,45 @@ namespace GameOfLife
             {
                 for (int j = 0; j < width ; j++)
                 {
-
                     int liveCells = 0; // Livecell neighbour count for each cell
 
                     for (int x = -1; x < 2; x++)
                     {
                         for (int y = -1; y < 2; y++)
                         {
-                            int cordinateX = i + x;
+                            int cordinateX = i + x; 
                             int cordinateY = j + y;
 
-                                
-                                 
+                            if (cordinateX > lenght - 1)
+                            {
+                                cordinateX = 0;
+                            }
 
-                                    if (cordinateY > width-1)
-                                    {
-                                        cordinateY = 0;
-                                    }
+                            if (cordinateX < 0)
+                            {
+                                cordinateX = lenght - 1;
+                            }
 
-                                    if (cordinateY < 0)
-                                    {
-                                        cordinateY = width -1;
+                            if (cordinateY > width-1)       // Calculate outside the borders
+                            {
+                                cordinateY = 0;
+                            }
 
-                                    }
+                            if (cordinateY < 0)
+                            {
+                                cordinateY = width -1;
+                            }
 
-                                    if (cordinateX > lenght - 1)
-                                    {
-                                        cordinateX = 0;
-                                    }
+                            liveCells = liveCells + GameField[cordinateX, cordinateY];
 
-                                    if (cordinateX < 0)
-                                    {
-                                        cordinateX = lenght - 1;
-
-                                    }
-
-                                    liveCells = liveCells + GameField[cordinateX, cordinateY];
-
-                              
                         }
-
                     }
-                   
                     liveCells = liveCells - GameField[i, j];
 
                     //Any live cell with fewer than two live neighbours dies, as if by underpopulation.
                     //Any live cell with two or three live neighbours lives on to the next generation.
                     //Any live cell with more than three live neighbours dies, as if by overpopulation.
                     //Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-
                     //1
                     if (GameField[i, j] == 1 && liveCells < 2) // Calculate cell state according to game rules
                     {
@@ -102,25 +86,19 @@ namespace GameOfLife
                     {
                         tempArr[i, j] = 1;
                     }
-
-
                 }
             }
 
 
             for (int i = 0; i < lenght; i++)    // Rewrite
             {
-
                 for (int j = 0; j < width; j++)
                 {
-
                     GameField[i, j] = tempArr[i, j];
                 }
-
             }
             return GameField;
         }
-
 
     }
 }
